cba23b8f11665c52eb3727ff1965689b46f46ad6
Rjw <jia1235590@gmail.com>
2026-06-21 14:08:47 +0900

    오류 수정

--- backend/hpm/hpm/settings.py
+++ backend/hpm/hpm/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv("DB_NAME", "hpm_db"),
         'USER': os.getenv("DB_USER", "root"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "1234"),
+        'PASSWORD': os.getenv("DB_PASSWORD", ""),
         'HOST': os.getenv("DB_HOST", "localhost"),
         'PORT': os.getenv("DB_PORT", "3306"),
         'OPTIONS': {'charset': 'utf8mb4'},
